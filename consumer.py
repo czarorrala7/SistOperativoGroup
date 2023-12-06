d153e190d8ba34cbbae68faee0e83bd8eb237aca
Isaac-1998 <149686764+Isaac-1998@users.noreply.github.com>
2023-12-05 21:53:29 -0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,97 +1,76 @@
-from  threading import Thread
-import threading
+import pika
 import json
 import os
-from lib.conexion import Conexion
-import signal
 
-class Repositorio(Thread):
-    """
-    Clase que almacena los datos obtenidos de la cola de mensajes en un repositorio del servidor
-      
-    Métodos:
-        __init__(): Constructor de la clase
-        run(user_id, contenido): Guarda los datos obtenidos en ficheros con extensión json
-        launch_tasks(n): Genera los n hilos de la clase
-    """
-    def __init__(self, user_id, contenido):
-        Thread.__init__(self)
-        self.name_file = user_id
-        self.contenido = contenido
-    
-    def run(self):
-        try:
-            # Intentamos guardar en la carpeta database
-            with open('./files/' + self.name_file + '.json', 'x') as f:
-                json.dump(self.contenido, f, ensure_ascii=False)
-        except FileExistsError:
-            # Añadir un sufijo al nombre del archivo hasta que encontremos un nombre que no esté en uso
-            suffix = 1
-            while True:
-                try:                    
-                    with open('./files/duplicados/' + self.name_file + '_' + str(suffix) + '.json', 'x') as f:
-                        json.dump(self.contenido, f)
-                    break  # Salir del ciclo una vez que el archivo se ha escrito con éxito
-                except FileExistsError:
-                    suffix += 1  # Incrementar el sufijo y probar con el siguiente número
+def callback(ch, method, properties, body):
+    try:
+        # Leer el archivo de configuración
+        with open('configuracion.json', 'r') as config_file:
+            configuracion = json.load(config_file)
 
+        # Encontrar el nodo con cargo "lider"
+        lider = next((nodo for nodo in configuracion if nodo["rol"] == "lider"), None)
 
+        if lider:
+            # Obtener el nombre de la carpeta del nodo líder
+            carpeta_lider = lider["nodo"]
 
-class ConsumerThread(Thread):
-    """
-    Clase que se conecta a RabbitMQ y envia los datos a la clase Repositorio para ser almacenados
-    
-    Atributos:
-        file_lock: Bloqueo de hilos
+            # Crear la carpeta si no existe
+            if not os.path.exists(carpeta_lider):
+                os.makedirs(carpeta_lider)
 
-    Métodos:
-        __init__(): Constructor de la clase
-        close_connection(): Controla el cierre de la conexión con RabbitMQ
-        launch_tasks(n): Genera los n hilos de la clase
-        callback(ch, method, properties, body): Controla el flujo de mensajes en la cola
-        run(): Crea el hilo de ejecución
-        signal_handler: Gestiona la interacción con el usuario por teclado
-    """
-    file_lock = threading.Lock()
+            # Decodificar el cuerpo del mensaje como JSON
+            mensaje_json = json.loads(body.decode('utf-8'))
 
-    def __init__(self):
-        self.conn = Conexion()
-        threading.Thread.__init__(self)
-        self.index_table = {}
-        self.file_pointer = 0
-    
-    def close_connection(self):
-        self.conn.close()
+            # Obtener la ruta completa del archivo dataBase.json en la carpeta del líder
+            ruta_archivo = os.path.join(carpeta_lider, 'dataBase.json')
 
-    def callback(self, ch, method, properties, body):
-        
-        message_dict = json.loads(body)
-        # Extrae el UUID y user_id
-        uuid_key, user_info = next(iter(message_dict.items()))
-        user_id = user_info["informacion_personal"]["user_id"]
+            # Inicializar la lista de datos existente
+            datos_existente = []
 
-        repositorio = Repositorio(user_id= user_id, contenido = message_dict)
-        repositorio.start()
-        # Acknowledge the message
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+            # Leer el contenido actual del archivo si existe
+            if os.path.exists(ruta_archivo):
+                try:
+                    with open(ruta_archivo, 'r') as archivo_existente:
+                        datos_existente = json.load(archivo_existente)
+                except (FileNotFoundError, json.JSONDecodeError):
+                    # Si hay algún error al cargar, simplemente mantener la lista vacía
+                    pass
 
-    def run(self):
-        self.conn.consuming(self.callback)
-        self.conn.close()
+            # Unir las listas existentes y el nuevo mensaje
+            datos_totales = datos_existente + mensaje_json
+            
+            # Escribir todos los datos en el nuevo archivo
+            with open(ruta_archivo, 'w') as archivo:
+                # Escribir la lista de datos_totales como una lista de objetos JSON
+                json.dump(datos_totales, archivo, indent=2)
 
-def signal_handler(sig, frame):
-    print('Presionaste Ctrl+C!')
-    for consumer in consumers:
-        consumer.close_connection()
-        consumer.join()  # Wait for threads to finish
-    os._exit(0)
+    except json.JSONDecodeError as e:
+        print("Error al decodificar el mensaje JSON:", str(e))
+
+def leer_cola_y_consolidar():
+    # Conexión con el servidor RabbitMQ
+    credentials = pika.PlainCredentials('michael', 'michael')
+    parameters = pika.ConnectionParameters('192.168.100.134', 5672, '/', credentials)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    # Declarar la cola desde la que leeremos mensajes
+    channel.queue_declare(queue='Monitor')  # No forzamos la propiedad 'durable'
+
+    # Configurar el callback para procesar los mensajes
+    channel.basic_consume(queue='Monitor', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Esperando mensajes. Presiona CTRL+C para salir.')
+
+    try:
+        # Iniciar la recepción de mensajes
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Cerrar la conexión al servidor RabbitMQ
+        connection.close()
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler)
-    consumers = [ConsumerThread() for _ in range(5)]
-    try:
-        for consumer in consumers:
-            consumer.start()
-    except Exception as e:
-        print(f"Error: {e}")
-
+    leer_cola_y_consolidar()
